eb77531a8106fceee88cab777824242d1f86b128
Iury Teixeira <113626147+iuryt7@users.noreply.github.com>
2024-09-09 17:30:50 -0300

    Add files via upload

--- exerc_2_go.go
+++ exerc_2_go.go
@@ -6,7 +6,6 @@ import (
 	"time"
 )
 
-// Função para calcular o fatorial de um número
 func fatorial(n int) int {
 	if n == 0 {
 		return 1
@@ -15,10 +14,8 @@ func fatorial(n int) int {
 }
 
 func main() {
-	// Configurando a semente para números aleatórios
 	rand.Seed(time.Now().UnixNano())
 
-	// Gerando um número pseudo-aleatório entre 0 e 10
 	num := rand.Intn(10)
 
 	fmt.Printf("Número gerado: %d\n", num)
